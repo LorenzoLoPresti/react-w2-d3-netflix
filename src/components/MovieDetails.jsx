53da0ae67efd5355489eed50cc04bd3d02700410
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-02-15 23:08:09 +0100

    commit

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { Container, Row, Col, ListGroup } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
+import { Link, useParams } from "react-router-dom";
+import Cards from "./Cards";
 
 const MovieDetails = (props) => {
-  const [movieArray, setMovieArray] = useState({});
+  const [movieArray, setMovieArray] = useState(null);
+  const [suggestion, setSuggestion] = useState(null);
+  const [suggestedMovie, setSuggestedMovie] = useState(null);
   const params = useParams();
 
   console.log("PARAMETRI", params);
@@ -18,54 +22,162 @@ const MovieDetails = (props) => {
       .then((movie) => {
         console.log("movie inside fetch", movie);
         setMovieArray(movie);
+        movie.Title.length >= 5
+          ? setSuggestion(movie.Title.substr(0, 5))
+          : setSuggestion(movie.Title);
       });
+    // fetch(`https://www.omdbapi.com/?apikey=9173a3c&s=${suggestion}`)
+    //   .then((response) => {
+    //     if (response.ok) {
+    //       return response.json();
+    //     }
+    //   })
+    //   .then((element) => setSuggestedMovie(element));
+  };
+
+  const suggestedFetch = () => {
+    console.log("suggestedFetch");
+    fetch(`https://www.omdbapi.com/?apikey=9173a3c&s=${suggestion}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((element) => setSuggestedMovie(element));
   };
 
   useEffect(() => {
+    console.log("useEffect");
     idFetch(`https://www.omdbapi.com/?apikey=9173a3c&i=${params.movieId}`);
-    console.log("movieOutsideFetch", movieArray);
-  }, []);
+  }, [params]);
+
+  useEffect(() => {
+    suggestedFetch();
+  }, [suggestion]);
+
+  console.log("render");
+  // console.log("movieOutsideFetch", movieArray);
+  // console.log("titolo", suggestion);
+  console.log("array", suggestedMovie);
 
   return (
-    <Container>
-      <Row>
-        <Col xs={12} md={5}>
-          <img src={movieArray.Poster} alt="" />
-        </Col>
-        <Col xs={12} md={7} className="text-white">
-          <ListGroup>
-            {/* 
+    <>
+      <Container
+        className="mt-5 mb-5 py-5 border border-secondary"
+        style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
+      >
+        {movieArray ? (
+          <Row>
+            <Col xs={12} md={5}>
+              <img src={movieArray.Poster} alt="" />
+            </Col>
+            <Col xs={12} md={7} className="text-white">
+              <ListGroup>
+                {/* 
             <ListGroup.Item>{movieArray.Actors}</ListGroup.Item>
           */}
-            <ListGroup.Item
-              style={{
-                backgroundColor: "#221f1f",
-                fontSize: "2rem",
-                fontWeight: "bold",
-                color: "red",
-              }}
-            >
-              {movieArray.Title}
-            </ListGroup.Item>
-            <ListGroup.Item
-              style={{ backgroundColor: "#221f1f", textAlign: "start" }}
-            >
-              {movieArray.Plot}
-            </ListGroup.Item>
-            <ListGroup.Item
-              style={{ backgroundColor: "#221f1f", textAlign: "start" }}
-            >
-              {movieArray.Genre}
-            </ListGroup.Item>
-            <ListGroup.Item
-              style={{ backgroundColor: "#221f1f", textAlign: "start" }}
-            >
-              {movieArray.Year}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
+                <ListGroup.Item
+                  style={{
+                    backgroundColor: "rgba(0,0,0,0)",
+                    fontSize: "2rem",
+                    fontWeight: "bold",
+                    color: "#E50914",
+                  }}
+                >
+                  {movieArray.Title}
+                </ListGroup.Item>
+                <div className="d-flex">
+                  <p
+                    style={{
+                      padding: "12px 0px 12px 20px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Plot:
+                  </p>
+                  <ListGroup.Item
+                    style={{
+                      backgroundColor: "rgba(0,0,0,0)",
+                      textAlign: "start",
+                    }}
+                  >
+                    {movieArray.Plot}
+                  </ListGroup.Item>
+                </div>
+                <div className="d-flex">
+                  <p
+                    style={{
+                      padding: "12px 0px 12px 20px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Genre:
+                  </p>
+                  <ListGroup.Item
+                    style={{
+                      backgroundColor: "rgba(0,0,0,0)",
+                      textAlign: "start",
+                    }}
+                  >
+                    {movieArray.Genre}
+                  </ListGroup.Item>
+                </div>
+                <div className="d-flex">
+                  <p
+                    style={{
+                      padding: "12px 0px 12px 20px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Year:
+                  </p>
+                  <ListGroup.Item
+                    style={{
+                      backgroundColor: "rgba(0,0,0,0)",
+                      textAlign: "start",
+                    }}
+                  >
+                    {movieArray.Year}
+                  </ListGroup.Item>
+                </div>
+              </ListGroup>
+            </Col>
+          </Row>
+        ) : (
+          <Spinner
+            animation="border"
+            role="status"
+            variant="danger"
+            className="mt-5"
+          ></Spinner>
+        )}
+      </Container>
+      <h2 className="pt-5">Potrebbero piacerti anche:</h2>
+      <Row className="p-5">
+        {suggestedMovie?.Search?.length > 0 ? (
+          suggestedMovie?.Search?.map((element) => {
+            return (
+              <Col xs={12} sm={6} md={3} xl={2}>
+                <Link to={"/movie-details/" + element.imdbID}>
+                  <Cards posterImg={element.Poster} />
+                </Link>
+              </Col>
+            );
+          })
+        ) : (
+          <Spinner
+            animation="border"
+            role="status"
+            variant="danger"
+            className="mt-5"
+          ></Spinner>
+        )}
+        {/* {suggestedMovie &&
+        suggestedMovie.map((element) => {
+          return <Cards />;
+        })} */}
       </Row>
-    </Container>
+    </>
   );
 };
 export default MovieDetails;
